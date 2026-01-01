1923a33f7c823e6ee4da6a6e5482b1a660e5c86e
heshankav <heshankavinda2023@gmail.com>
2026-01-01 23:10:48 +0530

    Add Book model to represent library books
    Introduced a Book class in the LibraryManagementSystem.Models namespace with properties for Id, Title, Author, ISBN, Quantity, and AvailableQuantity to encapsulate book details for the library management system.

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string ISBN { get; set; }
+		public int Quantity { get; set; }
+		public int AvailableQuantity { get; set; }
+	}
+}
