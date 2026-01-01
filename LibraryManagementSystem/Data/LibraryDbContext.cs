1083e90feaf85ec96ef42f305adfea644b5b78d0
heshankav <heshankavinda2023@gmail.com>
2026-01-02 00:10:30 +0530

    Add LibraryDbContext with DbSets for core entities
    Introduced LibraryDbContext inheriting from DbContext to manage Books, Members, and IssueRecords entities using Entity Framework Core. Included necessary using directives and a constructor accepting DbContextOptions for configuration.

---
+++ LibraryManagementSystem/Data/LibraryDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class LibraryDbContext : DbContext
+{
+	public LibraryDbContext(DbContextOptions options) : base(options) { }
+
+	public DbSet<Book> Books { get; set; }
+	public DbSet<Member> Members { get; set; }
+	public DbSet<IssueRecord> IssueRecords { get; set; }
+}
